4b655800a0b3cc662f59cde7a0104aee8ba37572
mahirlabibdihan <mahirlabibdihan@gmail.com>
2022-06-18 21:56:36 +0600

    Bug fixes - may be one last time

--- Offline/04-Max-Flow/1905072/BaseballElimination.cpp
+++ Offline/04-Max-Flow/1905072/BaseballElimination.cpp
@@ -2,19 +2,8 @@
 #include <iostream>
 #include <vector>
 #include <iomanip>
-#include <algorithm>
 #include "FordFulkerson.hpp"
 using namespace std;
-
-/*
-      We will try to find the best case scenerio of "X winning the division".
-      Assumptions:
-      X has won all left matches against teams in and out of its division.
-      Y (All other teams except X) has lost all matches against teams out of its division.
-      Now we will simulate matches between all the teams in X's division (except X). And try to find if it is possible for X to win the division for a particular results of all the matches.
-
-      X can win at most w[x]+r[x] matches. So no other team in X's division should win more than w[x]+r[x]. So we will limit i-th teams maximum number of wins to w[x]+r[x]-w[i]. If this is possible to finish all the remaining games under this condition, then X is not eliminated yet. Otherwise X is eliminated.
-       */
 
 void printNames(vector<string> names, int n)
 {
@@ -55,60 +44,43 @@ void printElimination(vector<string> name, vector<int> w, vector<int> r, vector<
     cout << name[x] << " is eliminated." << endl;
     cout << "They can win at most " << w[x] << " + " << r[x] << " = " << w[x] + r[x] << " games." << endl;
     printNames(names, nTeam);
-    cout << (names.size() == 1 ? " has" : " have")
-         << " won a total of " << nWins << " games." << endl;
+    cout << " have won a total of " << nWins << " games." << endl;
     cout << "They play each other " << nLeft << " times." << endl;
     cout << "So on average, each of the teams wins " << (nWins + nLeft) << "/" << nTeam << " = " << (1.0 * (nWins + nLeft) / nTeam) << " games." << endl
          << endl;
 }
 void solve(vector<string> name, vector<int> w, vector<int> r, vector<vector<int>> g, int n)
 {
-
-    // Create graph
-    // 0 to n-1: Teams node
-    // n to n + n_games - 1: Games node
     int n_games = (n * (n - 1) / 2);
     int V = 2 + n_games + n; // Total nodes
     int s = V - 2;           // Source node
     int t = V - 1;           // Sink node
-    int maxWins = max_element(w.begin(), w.end()) - w.begin();
+    vector<pair<int, int>> adj[V];
+    int max_flow = 0;
+    // Create graph
+    // 0 to n-1: Teams node
+    // n to n + n_games - 1: Games node
+    for (int i = 0, node_id = n; i < n; i++)
+    {
+        for (int j = i + 1; j < n; j++, node_id++)
+        {
+            adj[s].push_back({node_id, g[i][j]});
+            adj[node_id].push_back({i, INT_MAX});
+            adj[node_id].push_back({j, INT_MAX});
+            max_flow += g[i][j];
+        }
+    }
+    for (int i = 0; i < n; i++)
+    {
+        adj[i].push_back({t, 0});
+    }
     for (int x = 0; x < n; x++)
     {
-        vector<bool> reachable(V);
-
-        // Check trivial solution
-        if (w[x] + r[x] < w[maxWins])
-        {
-            reachable[maxWins] = true;
-            printElimination(name, w, r, reachable, x, n, g);
-            continue;
-        }
-
-        vector<pair<int, int>> adj[V];
-
         for (int i = 0; i < n; i++)
         {
-            if (i != x)
-            {
-                adj[i].push_back({t, w[x] + r[x] - w[i]});
-            }
+            adj[i][0].second = w[x] + r[x] - w[i];
         }
-
-        int max_flow = 0;
-        for (int i = 0, node_id = n; i < n; i++)
-        {
-            for (int j = i + 1; j < n; j++, node_id++)
-            {
-                if (i != x && j != x)
-                {
-                    adj[s].push_back({node_id, g[i][j]});
-                    adj[node_id].push_back({i, INT_MAX});
-                    adj[node_id].push_back({j, INT_MAX});
-                    max_flow += g[i][j];
-                }
-            }
-        }
-
+        vector<bool> reachable(V);
         int flow = fordFulkerson(adj, V, s, t, reachable);
         if (flow < max_flow)
         {
